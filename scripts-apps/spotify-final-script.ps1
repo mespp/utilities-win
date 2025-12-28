591cf68530c032dfc4b7e314d0ef07963c9ae159
mespp <62350991+mespp@users.noreply.github.com>
2025-12-28 21:25:35 +0100

    script not working cuz spotx has sum problem with Spotify.dll.bak solving it other day

--- scripts-apps/spotify-final-script.ps1
+++ scripts-apps/spotify-final-script.ps1
@@ -1,11 +1,31 @@
-# Ruta a tu archivo .bat
-$rutaBAT = "misc/unistall-spotify.bat"
+# uninstall current spotify
+Start-Process -FilePath "misc/uninstall-spotify.bat" -Wait
 
-# Ejecutar el .bat en una nueva ventana y esperar a que termine
-Start-Process -FilePath $rutaBAT -Wait
+# download new spotify
+$URL = "https://github.com/mespp/utilities-win/releases/download/apps/spotify-1.2.8.923.exe"
+$AppsFolder = Join-Path -Path $PSScriptRoot -ChildPath "apps"
 
-# Ejecutar como administrador
-Start-Process -FilePath $rutaBAT -Verb RunAs -Wait
+if (-not (Test-Path $AppsFolder)) { New-Item -ItemType Directory -Path $AppsFolder | Out-Null }
+cd $AppsFolder
 
-# Ejecutar con argumentos
-Start-Process -FilePath $rutaBAT -ArgumentList "param1", "param2" -Wait
+Start-Process -FilePath "curl.exe" -ArgumentList "-L -O $URL" -Wait
+
+# install new spotify
+Start-Process -FilePath "$AppsFolder/spotify-1.2.8.923.exe" 
+Start-Sleep -Seconds 10
+
+# close spotify
+Stop-Process -Name "Spotify" -Force -ErrorAction SilentlyContinue
+
+# falso dllbak
+cd $env:APPDATA\Spotify
+New-Item -ItemType File -Name Spotify.dll.bak
+
+# install spotx
+iex "& { $(iwr -useb 'https://raw.githubusercontent.com/SpotX-Official/SpotX/refs/heads/main/run.ps1') } -new_theme" 
+
+# install spicetify
+iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex 
+
+# pause updates with spicetify
+spicetify spotify-updates block
